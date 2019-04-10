6a99051c9b0f9e9104d261f192c01a8992a37bd5
Léo Sumi <pro.leosumi@gmail.com>
2019-04-10 15:45:34 +0200

    Add goupstate example and put the liftoff example in a separate directoy

--- example/build-example.sh
+++ example/build-example.sh
@@ -2,5 +2,10 @@
 
 set -e
 
-pandoc --template ../rss-template.xml feed.yml -o feed.xml
-sed -i '/^$/d' feed.xml
+pandoc --template ../rss-template.xml liftoff/feed.yml -o liftoff/feed.xml
+sed -i '/^$/d' liftoff/feed.xml
+xmllint --format liftoff/feed.xml --output liftoff/feed.xml
+
+pandoc --template ../rss-template.xml goupstate/feed.yml -o goupstate/feed.xml
+sed -i '/^$/d' goupstate/feed.xml
+xmllint --format goupstate/feed.xml --output goupstate/feed.xml
